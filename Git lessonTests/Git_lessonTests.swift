b1364fe248aa849b924e414532fc000ef62cea2b
ManBand <alapchev@yandex.ru>
2024-10-21 17:21:53 +0300

    Initial Commit

---
+++ Git lessonTests/Git_lessonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Git_lessonTests.swift
+//  Git lessonTests
+//
+//  Created by Пупка on 21.10.2024.
+//
+
+import Testing
+@testable import Git_lesson
+
+struct Git_lessonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
